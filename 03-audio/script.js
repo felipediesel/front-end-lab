44c4f4e51e76a84bf978b91c8c2015a9d24dda24
Felipe Diesel <felipediesel@gmail.com>
2016-11-12 18:03:37 -0200

    03: Adds option for line visualizer.

--- 03-audio/script.js
+++ 03-audio/script.js
@@ -1,5 +1,12 @@
 class AudioGraphic {
-  constructor(src) {
+  constructor(src, type = 'bars') {
+    const typeFunc = `render${type.charAt(0).toUpperCase() + type.slice(1)}`;
+    if (this[typeFunc] === undefined) {
+      throw new Error(`There is no ${typeFunc} available`);
+    }
+
+    this.renderGraph = this[typeFunc];
+
     this.audio = new Audio();
     this.audio.src = src;
     this.audio.autoplay = true;
@@ -59,7 +66,30 @@ class AudioGraphic {
 
   render() {
     this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.renderGraph();
+  }
 
+  renderLine() {
+    this.canvasContext.beginPath();
+
+    const data = this.getData();
+
+    data.forEach((step, i) => {
+      const x = (i * this.canvas.width) / data.length;
+      const y = this.canvas.height - ((step * this.canvas.height) / this.analyser.fftSize);
+      if (i === 0) {
+        this.canvasContext.moveTo(x, y);
+      } else {
+        this.canvasContext.lineTo(x, y);
+      }
+    });
+
+    this.canvasContext.lineWidth = 3;
+    this.canvasContext.strokeStyle = '#2A68AB';
+    this.canvasContext.stroke();
+  }
+
+  renderBars() {
     const data = this.getData();
 
     this.canvasContext.fillStyle = '#2A68AB';
